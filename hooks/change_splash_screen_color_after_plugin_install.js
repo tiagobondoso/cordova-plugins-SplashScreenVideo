100027d73a79cc63e3d47d00a669b78f371ec910
Paulo César <paulocesar@Paulos-Laptop.local>
2023-08-02 14:25:37 +0200

    fix hook to change theme color background color

--- hooks/change_splash_screen_color_after_plugin_install.js
+++ hooks/change_splash_screen_color_after_plugin_install.js
@@ -6,35 +6,38 @@ const path = require('path');
 const themesXmlPath = path.join('platforms', 'android', 'app', 'src', 'main', 'res', 'values', 'themes.xml');
 
 // Função para buscar e substituir a cor no arquivo themes.xml
-module.exports = function(context) {
-    fs.readFile(themesXmlPath, 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error reading themes.xml:', err);
-          return;
+function changeSplashScreenColor() {
+  fs.readFile(themesXmlPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading themes.xml:', err);
+      return;
+    }
+
+    const args = process.argv
+    var hexColor;
+
+    for (const arg of args) {  
+        if (arg.includes('SPLASH_BACKGROUND_COLOR')){
+            var stringArray = arg.split("=");
+            hexColor = stringArray.slice(-1).pop();
         }
-    
-        const args = process.argv
-        var hexColor;
-    
-        for (const arg of args) {  
-            if (arg.includes('SPLASH_BACKGROUND_COLOR')){
-                var stringArray = arg.split("=");
-                hexColor = stringArray.slice(-1).pop();
-            }
-        }
-    
-       console.log(`✅ New hex color :: '${hexColor}' in 'colors.xml'.`);
-    
-        const regex = /<item name="windowSplashScreenBackground">@color\/cdv_splashscreen_background<\/item>/g;
-        const newContent = data.replace(regex, `<item name="windowSplashScreenBackground">${hexColor}</item>`);
-    
-        fs.writeFile(themesXmlPath, newContent, 'utf8', (err) => {
-          if (err) {
-            console.error('Error writing to themes.xml:', err);
-            return;
-          }
-    
-          console.log(`Splash screen background color has been changed to '${hexColor}' in 'themes.xml'.`);
-        });
-      });
+    }
+
+   console.log(`✅ New hex color :: '${hexColor}' in 'colors.xml'.`);
+
+    const regex = /<item name="windowSplashScreenBackground">@color\/cdv_splashscreen_background<\/item>/g;
+    const newContent = data.replace(regex, `<item name="windowSplashScreenBackground">${hexColor}</item>`);
+
+    fs.writeFile(themesXmlPath, newContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing to themes.xml:', err);
+        return;
+      }
+
+      console.log(`Splash screen background color has been changed to '${hexColor}' in 'themes.xml'.`);
+    });
+  });
 }
+
+// Chamar a função para alterar a cor
+changeSplashScreenColor();
